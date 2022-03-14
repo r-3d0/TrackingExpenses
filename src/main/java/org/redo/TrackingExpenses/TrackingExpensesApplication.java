47e98cae1d63fbbca68608f06f1bbe2a79c6b3e6
r3d0 <c0rr3ubr0ssa@gmail.com>
2022-03-14 21:48:27 +0100

    initial commit

---
+++ src/main/java/org/redo/TrackingExpenses/TrackingExpensesApplication.java
@@ -0,0 +1,13 @@
+package org.redo.TrackingExpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/redo/TrackingExpenses/TrackingExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.redo.TrackingExpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
